0a85dd9ffedb8bc4ec49f1836bdd981642720d6b
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2023-08-31 15:14:10 +0300

    Add files via upload

---
+++ Lab3/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Departments;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\users;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('/users',users::class);
+Route::resource('/departments',Departments::class);
